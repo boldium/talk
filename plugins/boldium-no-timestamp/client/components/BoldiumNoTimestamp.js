36f9d6903fb34d9bd129c5d66327e5ef6b4434e1
eric mikkelsen <eric@ericmikkelsen.com>
2018-09-25 10:44:18 -0700

    feat: adds sparkt custom plugins

---
+++ plugins/boldium-no-timestamp/client/components/BoldiumNoTimestamp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class BoldiumNoTimestamp extends React.Component {
+  render() {
+    return <time className="BoldiumNoTimestamp" hidden />;
+    // get it, becaue the time is hidden?
+  }
+}
+
+export default BoldiumNoTimestamp;
---
+++ plugins/boldium-no-timestamp/client/index.js
@@ -0,0 +1,7 @@
+import BoldiumNoTimestamp from './components/BoldiumNoTimestamp';
+
+export default {
+  slots: {
+    commentTimestamp: [BoldiumNoTimestamp],
+  },
+};
